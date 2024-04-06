ed2c7e0fadb968fceac87591e5c8b4e12f8d37fd
sgtslaughta <richardsoto1010@gmail.com>
2024-04-06 13:52:18 -0400

    Changed folders and Dockerfile

--- docker-imgs/custom_jammy/p10k-config/.zshrc
+++ docker-imgs/custom_jammy/p10k-config/.zshrc
@@ -133,4 +133,4 @@ alias c='clear'
 alias gh='history | grep'
 alias l='ls -lha'
 
-xfconf-query -c xsettings -p /Net/ThemeName -s "Greybird-dark"
+# xfconf-query -c xsettings -p /Net/ThemeName -s "Greybird-dark"
--- docker-imgs/install_scripts/cold_storage_check.sh
+++ docker-imgs/install_scripts/cold_storage_check.sh
@@ -3,5 +3,5 @@
 # Check if /cold_storage exists
 if [ -d "/cold_storage" ]; then
     # Create a symbolic link to /cold_storage on the user's desktop
-    ln -s "/cold_storage" "$HOME/Desktop/ColdStorageShortcut"
+    ln -s "/cold_storage" "$HOME/Desktop/cold_storage"
 fi
