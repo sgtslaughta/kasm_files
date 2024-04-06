f3c5a6faa1cef089bccfa84ecb161dfd773ebc65
sgtslaughta <richardsoto1010@gmail.com>
2024-04-06 14:00:58 -0400

    Changed folders and Dockerfile

--- docker-imgs/install_scripts/cold_storage_check.sh
+++ docker-imgs/install_scripts/cold_storage_check.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/bin/zsh
 
 # Check if /cold_storage exists
 if [ -d "/cold_storage" ]; then
     # Create a symbolic link to /cold_storage on the user's desktop
     ln -s "/cold_storage" "$HOME/Desktop/cold_storage"
 fi
+
+# Set the Greybird-dark theme
+xfconf-query -c xsettings -p /Net/ThemeName -s 'Greybird-dark'
